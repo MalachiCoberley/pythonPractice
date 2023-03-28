d5c2d82035f707d26704a58860ca4c2b21c09db7
MalachiRutter <malachicoberley@gmail.com>
2023-03-28 15:39:39 -0700

    add first encounter

--- gameLearning.py
+++ gameLearning.py
@@ -4,11 +4,16 @@ import random
 def play():
     life = 3
     luck = random.randint(0,3)
+    backpack = {}
     print("""Hello adventurer. Choose an item for your quest.
           """)
     item = getItem()
     print(f"Ah, a {item}, what an excellent choice!")
     print("Well buckle up! here comes your first challenge...")
+    
+    #encounters will return truthy if they are completed and falsey if they are failed
+    if getRandomEncounter(item, luck):
+        print("wahoo")
     
 def getItem():
     itemNumber = input("""Enter the number that corresponds with the item you want
@@ -26,6 +31,28 @@ def getItem():
             item = "Pool Noodle"
     return item
 
+def getRandomEncounter(item, luck):
+    #hmmm... threading params. suss
+    encounterOldMan(item, luck)
+
+def encounterOldMan(item, luck):
+    modifier = luck + 0
+    print("An old man in a bathrobe emerges from the shadows.")
+    print("He wields a can of beans and a newspaper.")
+    if item == "Sword":
+        print("Before he can react, you swing your sword, missing him entirely but cleanly opening his can of beans from the top")
+        print("Satisfied, he leaves.")
+        return True
+    elif item == "Pool Noodle":
+        print("'Nice pool noodle, nerd', says the old man. 'Clearly you're not a great listener'.")
+        modifier -= 2
+    print(f"You swing your {item} at him...")
+    if modifier > 0:
+        print(f"An echoing thud hits your ears and the old man explodes into a million pieces of old-man confetti as your {item} drives through him.")
+        return True
+    else:
+        print("Embarassingly, you miss and catch a can of beans to the back of the head.")
+        return False
 
 #Start the game
 play()
