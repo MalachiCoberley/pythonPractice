eec4293e42a91a8d6dc59815a71379d15e0589fd
MalachiRutter <malachicoberley@gmail.com>
2023-04-07 07:47:43 -0700

    I'm honestly not sure what changed here

--- cliMon.py
+++ cliMon.py
@@ -4,18 +4,21 @@ import random
 ### Constants
 TYPES = ['Rock', 'Paper', 'Scissors']
 EFFECTS = ['Inaction', 'Switch', 'Drain', 'Swap']
-ATTACK_TYPES = ['Strong', 'Quick', 'Stun', 'Hit and Run', 'Poison', 'Scare']
+ATTACK_TYPES = ['Standard', 'Strong', 'Quick', 'Stun', 'Hit and Run', 'Poison', 'Scare']
+
 
 #This approach to handling moves may be dumb.... we will see.
 def generateMoves() -> list:
     moves = []
-    moves.append(Move(ATTACK_TYPES[0], 30, 2))
-    moves.append(Move(ATTACK_TYPES[1], 15, 0))
-    moves.append(Move(ATTACK_TYPES[2], 10, 1, EFFECTS[0]))
-    moves.append(Move(ATTACK_TYPES[3], 20, 1, EFFECTS[1]))
-    moves.append(Move(ATTACK_TYPES[4], 10, 1, EFFECTS[2]))
-    moves.append(Move(ATTACK_TYPES[5], 10, 1, EFFECTS[3]))
+    moves.append(Move(ATTACK_TYPES[0], 20, 1))
+    moves.append(Move(ATTACK_TYPES[1], 30, 2))
+    moves.append(Move(ATTACK_TYPES[2], 15, 0))
+    moves.append(Move(ATTACK_TYPES[3], 10, 1, EFFECTS[0]))
+    moves.append(Move(ATTACK_TYPES[4], 15, 1, EFFECTS[1]))
+    moves.append(Move(ATTACK_TYPES[5], 10, 1, EFFECTS[2]))
+    moves.append(Move(ATTACK_TYPES[6], 10, 1, EFFECTS[3]))
     return moves
+
 
 class Player:
     def __init__(self) -> None:
@@ -25,13 +28,15 @@ class Player:
     
 class Monster:
     def __init__(self) -> None:
+        self.name = input("What is my name? \n")
         self.baseHp = random.randint(100, 200)
         self.attack = random.randint(5, 25)
         self.defense = random.randint(5, 25)
         self.type = TYPES[random.randint(0,2)]
-        self.moves = [()]
+        self.moves = [ALL_MOVES[random.randint(0,2)], ALL_MOVES[random.randint(3,6)]]
 
     def getType(self):
+        print(self.moves)
         return self.type
 
 #
@@ -42,4 +47,12 @@ class Move:
         self.priority = priority
         self.effect = effect
     
+#confirmed. this approach is dumb and runtime gets fucky. 
+ALL_MOVES = generateMoves()
+
+class Game:
+    pass
+
+
 pops = Monster()
+pops.getType()
