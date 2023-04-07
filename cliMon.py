772c59b562c9337ade922c23e275e88ed2ed37e7
MalachiRutter <malachicoberley@gmail.com>
2023-04-06 17:49:01 -0700

    Add move class and function to create avaliable moves

--- cliMon.py
+++ cliMon.py
@@ -3,17 +3,28 @@ import random
 
 ### Constants
 TYPES = ['Rock', 'Paper', 'Scissors']
+EFFECTS = ['Inaction', 'Switch', 'Drain', 'Swap']
+ATTACK_TYPES = ['Strong', 'Quick', 'Stun', 'Hit and Run', 'Poison', 'Scare']
 
-# random 2 moves. priority attack, switch attack, power attack, status attack
+#This approach to handling moves may be dumb.... we will see.
+def generateMoves() -> list:
+    moves = []
+    moves.append(Move(ATTACK_TYPES[0], 30, 2))
+    moves.append(Move(ATTACK_TYPES[1], 15, 0))
+    moves.append(Move(ATTACK_TYPES[2], 10, 1, EFFECTS[0]))
+    moves.append(Move(ATTACK_TYPES[3], 20, 1, EFFECTS[1]))
+    moves.append(Move(ATTACK_TYPES[4], 10, 1, EFFECTS[2]))
+    moves.append(Move(ATTACK_TYPES[5], 10, 1, EFFECTS[3]))
+    return moves
 
 class Player:
-    def __init__(self):
+    def __init__(self) -> None:
         # seed team of 3 randos
        self.team = []
 
     
 class Monster:
-    def __init__(self):
+    def __init__(self) -> None:
         self.baseHp = random.randint(100, 200)
         self.attack = random.randint(5, 25)
         self.defense = random.randint(5, 25)
@@ -25,7 +36,10 @@ class Monster:
 
 #
 class Move:
-    
+    def __init__(self, name, power, priority, effect = None) -> None:
+        self.name = name
+        self.power = power
+        self.priority = priority
+        self.effect = effect
     
 pops = Monster()
-print(pops.getType())
