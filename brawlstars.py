4fa254453ebda5c38412d427104149c0486e50aa
MalachiRutter <malachicoberley@gmail.com>
2023-04-27 16:58:41 -0700

    functioning script for requesting player battle logs

--- brawlstars.py
+++ brawlstars.py
@@ -1,5 +1,17 @@
 #ya boi plays too much brawlstars, so i'm going to practice with the API.
 
+import urllib3
+
 from api_keys import TEST_API_TOKEN
 
-print(TEST_API_TOKEN)
+MAL = '%2328RP22UYC'
+CAL = '%2320CL0P8C0'
+
+http = urllib3.PoolManager()
+
+headers = {'Authorization': f'Bearer {TEST_API_TOKEN}'}
+url = f'https://api.brawlstars.com/v1/players/{MAL}/battlelog'
+
+resp = http.request('GET', url, headers=headers)
+
+print(resp.data)
