585b465717b34b6200c14a6b5e2a3519f7d9eb3c
MalachiRutter <malachicoberley@gmail.com>
2023-04-28 15:17:21 -0700

    test

--- brawl_db.py
+++
@@ -1,9 +0,0 @@
-# This is going to be a real prototype-ish way of managing migrations.
-# I'll """ string up """ commands that don't need to be run anymore.
-
-import sqlite3
-con = sqlite3.connect("brawl.db")
-cur = con.cursor()
-
-cur.execute("CREATE TABLE matches(timestamp, player_tag,player_name,brawler,game_mode,result,star_player, UNIQUE(timestamp,player_tag));")
-
--- brawl_query.py
+++
@@ -1,8 +0,0 @@
-#testing ground for querying the db
-import sqlite3
-
-con = sqlite3.connect("brawl.db")
-cur = con.cursor()
-
-res = cur.execute("Select * from matches")
-print(res.fetchall())
