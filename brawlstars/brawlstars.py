15345ac4fd1401e2baf9250635e8a43932609067
MalachiRutter <malachicoberley@gmail.com>
2023-04-28 17:03:53 -0700

    broke into functions and run pull for me and caleb

--- brawlstars/brawlstars.py
+++ brawlstars/brawlstars.py
@@ -13,64 +13,72 @@ cur = con.cursor()
 
 MAL_URL = '%2328RP22UYC'
 MAL_PLAIN = '#28RP22UYC'
+MAL_NAME = 'SleezyP'
 CAL_URL = '%2320CL0P8C0'
 CAL_PLAIN = '#20CL0P8C0'
+CAL_NAME = 'OreganoSpindle'
+
 http = urllib3.PoolManager()
-
 headers = {'Authorization': f'Bearer {TEST_API_TOKEN}'}
-url = f'https://api.brawlstars.com/v1/players/{MAL_URL}/battlelog'
 
-resp = http.request('GET', url, headers=headers)
-jason = json.loads(resp.data)
+def request_brawl_matches(player_url_tag):
+    url = f'https://api.brawlstars.com/v1/players/{player_url_tag}/battlelog'
+    resp = http.request('GET', url, headers=headers)
+    jason = json.loads(resp.data)
+    return jason
 
 #iterate through each match to fetch needed info
 #TODO: may need to account for other non-standard matches like weekend-only modes.
-for match in jason['items']:
-    timestamp = match['battleTime']
-    game_mode = match['battle']['mode']
-    result = ""
-    star_player = False
-    brawler = ""
-    if is_showdown(game_mode):
-        result = calculate_showdown_result(game_mode, match['battle']['rank'])
-        if game_mode == "soloShowdown":
+def write_json_to_db(jason, player_tag, player_name):
+    for match in jason['items']:
+        timestamp = match['battleTime']
+        game_mode = match['battle']['mode']
+        result = ""
+        star_player = False
+        brawler = ""
+        if is_showdown(game_mode):
+            result = calculate_showdown_result(game_mode, match['battle']['rank'])
+            if game_mode == "soloShowdown":
+                for player in match['battle']['players']:
+                    if player["tag"] == player_tag:
+                                brawler = player['brawler']['name']
+            else:
+                while brawler == "":
+                    for team in match['battle']['teams']:
+                        for player in team:
+                            if player["tag"] == player_tag:
+                                brawler = player['brawler']['name']
+        #For duels, I'm just grabbing the first brawler. The info for this mode kind of sucks.
+        elif game_mode == "duels":
+            result = match['battle']['result']
             for player in match['battle']['players']:
-                if player["tag"] == MAL_PLAIN:
-                            brawler = player['brawler']['name']
+                    if player["tag"] == player_tag:
+                                brawler = player['brawlers'][0]['name']
         else:
+            result = match['battle']['result']
+            if match['battle']['starPlayer'] == None:
+                pass
+            elif match['battle']['starPlayer']['tag'] == player_tag:
+                star_player = True
             while brawler == "":
                 for team in match['battle']['teams']:
                     for player in team:
-                        if player["tag"] == MAL_PLAIN:
+                        if player["tag"] == player_tag:
                             brawler = player['brawler']['name']
-    #For duels, I'm just grabbing the first brawler. The info for this mode kind of sucks.
-    elif game_mode == "duels":
-        result = match['battle']['result']
-        for player in match['battle']['players']:
-                if player["tag"] == MAL_PLAIN:
-                            brawler = player['brawlers'][0]['name']
-    else:
-        result = match['battle']['result']
-        if match['battle']['starPlayer'] == None:
-             pass
-        elif match['battle']['starPlayer']['tag'] == MAL_PLAIN:
-            star_player = True
-        while brawler == "":
-            for team in match['battle']['teams']:
-                for player in team:
-                    if player["tag"] == MAL_PLAIN:
-                        brawler = player['brawler']['name']
-    print(timestamp, " : ",game_mode, " : ",result, " : ",star_player, " : ",brawler)
-    #write to Sqlite DB
-    data = (timestamp, MAL_PLAIN,'SleezyP',brawler,game_mode,result,star_player)
-    try:
-        cur.execute("INSERT INTO matches VALUES(?,?,?,?,?,?,?)", data)
-        con.commit()
-        print("match logged")
-    except:
-        print("already written")
-        pass
+        print(timestamp, " : ",game_mode, " : ",result, " : ",star_player, " : ",brawler)
+        #write to Sqlite DB
+        data = (timestamp,player_tag,player_name,brawler,game_mode,result,star_player)
+        try:
+            cur.execute("INSERT INTO matches VALUES(?,?,?,?,?,?,?)", data)
+            con.commit()
+            print("match logged")
+        except:
+            print("already written")
+            pass
 
+
+write_json_to_db(request_brawl_matches(CAL_URL),CAL_PLAIN,CAL_NAME)
+write_json_to_db(request_brawl_matches(MAL_URL),MAL_PLAIN,MAL_NAME)
 
 
 #battleLogs.to_json('./temp.json')
