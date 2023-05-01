00cab69cb67520e2c29c8cda842bc51bc634084f
MalachiRutter <malachicoberley@gmail.com>
2023-04-30 21:11:50 -0700

    Extract DB ops into separate function

--- brawlstars/brawlstars.py
+++ brawlstars/brawlstars.py
@@ -18,31 +18,41 @@ CAL_URL = '%2320CL0P8C0'
 CAL_PLAIN = '#20CL0P8C0'
 CAL_NAME = 'OreganoSpindle'
 
-STUPID_WEEKEND_MATCHES = ['bigGame', 'roboRumble', 'bossFight']
+WEEKEND_GAME_MODES = ['bigGame', 'roboRumble', 'bossFight']
+SHOWDOWN_GAME_MODES = ["duoShowdown", "soloShowdown"]
 
 http = urllib3.PoolManager()
 headers = {'Authorization': f'Bearer {TEST_API_TOKEN}'}
 
-def request_brawl_matches(player_url_tag):
+def request_brawl_matches(player_url_tag) -> json:
     url = f'https://api.brawlstars.com/v1/players/{player_url_tag}/battlelog'
     resp = http.request('GET', url, headers=headers)
     jason = json.loads(resp.data)
     return jason
 
-#iterate through each match to fetch needed info
-#TODO: may need to account for other non-standard matches like weekend-only modes.
-def write_json_to_db(jason, player_tag, player_name):
-    for match in jason['items']:
-        #Skip over the record if it's a weekend mode
-        if match['battle']['mode'] in STUPID_WEEKEND_MATCHES:
+def write_data_to_db(data_list) -> None:
+    for data in data_list:
+        try:
+            cur.execute("INSERT INTO matches VALUES(?,?,?,?,?,?,?)", data)
+            con.commit()
+            print(f"match logged for {data[2]}")
+        except:
             pass
+
+def extract_match_data(jason, player_tag, player_name) -> list:
+    extracted_data = []
+    for match in jason['items']:
+        timestamp = match['battleTime']
+        game_mode = match['battle']['mode']
+        result = ""
+        star_player = False
+        brawler = ""
+        #Skip over the record if it's a weekend mode
+        if game_mode in WEEKEND_GAME_MODES:
+            pass
+        #Otherwise extract game info and write the match to the DB
         else:
-            timestamp = match['battleTime']
-            game_mode = match['battle']['mode']
-            result = ""
-            star_player = False
-            brawler = ""
-            if is_showdown(game_mode):
+            if game_mode in SHOWDOWN_GAME_MODES:
                 result = calculate_showdown_result(game_mode, match['battle']['rank'])
                 if game_mode == "soloShowdown":
                     for player in match['battle']['players']:
@@ -71,18 +81,14 @@ def write_json_to_db(jason, player_tag, player_name):
                         for player in team:
                             if player["tag"] == player_tag:
                                 brawler = player['brawler']['name']
-            #write to Sqlite DB
+            #return data tuple for DB
             data = (timestamp,player_tag,player_name,brawler,game_mode,result,star_player)
-            try:
-                cur.execute("INSERT INTO matches VALUES(?,?,?,?,?,?,?)", data)
-                con.commit()
-                print(f"match logged for {player_name}")
-            except:
-                pass
+            extracted_data.append(data)
+    return extracted_data
 
 
-write_json_to_db(request_brawl_matches(CAL_URL),CAL_PLAIN,CAL_NAME)
-write_json_to_db(request_brawl_matches(MAL_URL),MAL_PLAIN,MAL_NAME)
+write_data_to_db(extract_match_data(request_brawl_matches(CAL_URL),CAL_PLAIN,CAL_NAME))
+write_data_to_db(extract_match_data(request_brawl_matches(MAL_URL),MAL_PLAIN,MAL_NAME))
 
 
 con.close()
