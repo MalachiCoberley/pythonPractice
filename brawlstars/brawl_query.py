ea16b69f16c76e9d90b56645446f29276cdbf2b9
MalachiRutter <malachicoberley@gmail.com>
2023-05-02 19:08:10 -0700

    db update and brawler plot size change

--- brawlstars/brawl_query.py
+++ brawlstars/brawl_query.py
@@ -12,7 +12,8 @@ matches = pd.read_sql_query('Select player_name Player, brawler Brawler, CASE re
 brawler_stats_by_player = pd.crosstab(values=matches['Result'], index=matches['Brawler'], columns=matches['Player'], aggfunc=np.sum)
 #Replace NaN with zero after setting the zero values to .01 to distinguish a "zero" from "no data"
 brawler_stats_by_player = brawler_stats_by_player.replace(0,.01).fillna(0)
-brawler_stats_by_player.plot.barh()
+brawler_stats_by_player.plot.barh(figsize=(20, 15), width=0.8)
+
 
 ### Show Top 3 Brawlers Per Player ###
 # This version of sqlite doesn't seem to like window functions, so i'm just doing two separate queries
