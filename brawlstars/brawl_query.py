2987bfb8b01dde476fff6d07b16196025b8441ff
MalachiRutter <malachicoberley@gmail.com>
2023-05-08 21:42:17 -0700

    mostly just db update

--- brawlstars/brawl_query.py
+++ brawlstars/brawl_query.py
@@ -9,13 +9,13 @@ from brawl_utils import convert_utc_to_az_datetime
 con = sqlite3.connect("brawl.db")
 cur = con.cursor()
 
-"""
 ### Bar Graph of Relative Win/Loss With Each Brawler ###
 matches = pd.read_sql_query('Select player_name Player, brawler Brawler, CASE result WHEN "victory" THEN 1 ELSE -1 END as Result from matches', con)
 brawler_stats_by_player = pd.crosstab(values=matches['Result'], index=matches['Brawler'], columns=matches['Player'], aggfunc=np.sum)
 #Replace NaN with zero after setting the zero values to .01 to distinguish a "zero" from "no data"
 brawler_stats_by_player = brawler_stats_by_player.replace(0,.01).fillna(0)
 brawler_stats_by_player.plot.barh(figsize=(20, 15), width=0.8)
+
 
 
 ### Show Top 3 Brawlers Per Player ###
@@ -26,8 +26,8 @@ cal_top_brawlers = pd.read_sql_query('Select player_name, brawler, count(brawler
 top_brawlers = [mal_top_brawlers, cal_top_brawlers]
 for df in top_brawlers:
     print(df)
-"""
 
+"""
 def performance_over_time(con):
     #player_name = input('What is your player name?')
     #brawler = input('What brawler do you want to view?').upper()
@@ -39,9 +39,7 @@ def performance_over_time(con):
     for idx, row in results.iterrows():
         print(convert_utc_to_az_datetime(row['timestamp']))
 
-# performance_over_time(con)
-
-cur.execute('Select brawl_dump from matches where player_name = "SleezyP" and brawler = "ASH" ')
-print(cur.fetchall())
+performance_over_time(con)
+"""
 
 con.close()
